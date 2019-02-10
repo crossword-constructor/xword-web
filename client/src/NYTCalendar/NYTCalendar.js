b35073407eb46fd50c52654dbdfcf2f6360b8c80
okputadora <okputadora@gmail.com>
2019-02-10 16:29:25 -0500

    display all dates

--- client/src/NYTCalendar/NYTCalendar.js
+++ client/src/NYTCalendar/NYTCalendar.js
@@ -3,7 +3,7 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 const NYTCalendar = () => {
-  const [currentMonth, setYear] = useState("/2/1942");
+  const [currentDate, Date] = useState("/2/1942");
 
   const FETCH_PUZZLES = gql`
     query Puzzle($id: ID) {
@@ -15,14 +15,53 @@ const NYTCalendar = () => {
     }
   `;
 
+  const monthMap = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+  };
+  const BuildYearList = function() {
+    let years = [];
+    for (let year = 1942; year <= 2019; year++) {
+      let months = [];
+      for (let month = 1; month <= 12; month++) {
+        months.push({
+          title: monthMap[month],
+          link: `${month}/${year}`
+        });
+      }
+      // console.log(years);
+      years.push({ year, months: months.reverse() });
+    }
+    return years;
+  };
+
+  let yearList = BuildYearList();
+  console.log(yearList);
   return (
     <div>
       <ol>
-        <li>1942</li>
-        <li>1943</li>
-        <li>1944</li>
+        {yearList.reverse().map(year => (
+          <li key={year.year}>
+            {year.year}
+            <ol>
+              {year.months.map(month => (
+                <li key={month.title}>{month.title}</li>
+              ))}
+            </ol>
+          </li>
+        ))}
       </ol>
-      <Query query={FETCH_PUZZLES} variables={{ id: 1 }}>
+      <Query query={FETCH_PUZZLES} variables={{ date: currentDate }}>
         {({ loading, error, data }) => {
           if (loading) return "Loading...";
           if (error) {
