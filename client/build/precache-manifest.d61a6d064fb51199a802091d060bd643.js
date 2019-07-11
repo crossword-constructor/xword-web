536e443c03f833ce88b9fed370fe8edfb26764bb
okputadora <okputadora@gmail.com>
2019-07-11 12:51:31 -0400

    adding build to deploy

---
+++ client/build/precache-manifest.d61a6d064fb51199a802091d060bd643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "798fd5a061056de0c0ccecd22fa1fdac",
+    "url": "/static/media/test.798fd5a0.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cebcf327d306de0d51c8",
+    "url": "/static/js/main.cebcf327.chunk.js"
+  },
+  {
+    "revision": "b710cc3b2624f021a683",
+    "url": "/static/js/1.b710cc3b.chunk.js"
+  },
+  {
+    "revision": "cebcf327d306de0d51c8",
+    "url": "/static/css/main.e5695948.chunk.css"
+  },
+  {
+    "revision": "3629e2997a934406df1e0eb8d361df2d",
+    "url": "/index.html"
+  }
+];
